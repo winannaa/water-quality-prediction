b991d9e1210ed1d94ae88f8bbcaf542763e69b8a
Wina Nur Annisa <117616393+winannaa@users.noreply.github.com>
2025-12-10 22:43:38 +0700

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("💧 Water Quality Prediction App")
+st.write("""
+Selamat datang di aplikasi prediksi kualitas air!
+
+Navigasi ada di sidebar →  
+- **Data & Info**  
+- **Model & Evaluasi**  
+- **Prediksi Kualitas Air**  
+""")
